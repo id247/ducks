07aa0998b2749c15822c8cd29cc6014654a8960c
Bazhenov Aleksandr <abazheno@t-systems.ru>
2018-09-14 19:49:55 +0300

    fixed proptypes import

--- src/components/features/Welcome/Welcome.js
+++ src/components/features/Welcome/Welcome.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MODES } from '../../../common/constants/mode';
 import { Label, Radio } from '../../common';
 
